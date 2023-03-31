3ea6186ef67beff44ff328aeb34429c9962bfc27
Vaishnavishah <shahvaishnavi2411@gmail.com>
2023-03-30 20:07:32 -0400

    Changes in tuits and app.js

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ import TuitsController
   from "./controllers/tuits/tuits-controller.js";
 
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors());
 app.use(express.json());
 TuitsController(app);
 HelloController(app)
 UserController(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
 
